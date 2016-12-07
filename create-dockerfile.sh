168ecf7b19fc1c7ce42b6f0907916c964f7f4199
1ambda <1amb4a@gmail.com>
2016-12-07 16:04:22 +0900

    feat: Add alpine, centos, ubuntu images

--- create-dockerfile.sh
+++ create-dockerfile.sh
@@ -1,26 +1,37 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 1 ];
+if [ $# -lt 2 ];
 then
-    echo "USAGE: $0 version (e.g. 0.7.0)"
+    echo "USAGE: $0 version linux platform"
+    echo "example: $0 0.7.0 alpine python"
     exit 1
 fi
 
-tag="[CREATE-DOCKERFILE]"
-version=$1
-dirName="bin-all-${version}"
+TAG="[CREATE-DOCKERFILE]"
+VERSION=$1
+LINUX=$2
+PLATFORM=$3
+POSTFIX=""
 
-if [ ! -d "$dirName" ]; then
-    echo "${tag} Creating Directory: ${dirName}"
-    mkdir -p ${dirName}
+if [[ ! "x${PLATFORM}" = "x" ]]; then
+   POSTFIX="_${PLATFORM}"
+fi
 
-    echo "${tag} Copying File: ${dirName}/Dockerfile"
-    cp Dockerfile.template ${dirName}/Dockerfile
+BASE_DIR="${LINUX}-${VERSION}${POSTFIX}"
+DOCKER_TAG="${LINUX}-${VERSION}${POSTFIX}"
 
-    echo "${tag} Set Version: ${version}"
-    sed -i '' -e "s/Z_VERSION=\"0.0.0\"/Z_VERSION=\"${version}\"/g" ${dirName}/Dockerfile
+if [ ! -d "$BASE_DIR" ]; then
+    echo "${TAG} Creating Directory: ${BASE_DIR}"
+    mkdir -p ${BASE_DIR}
+
+    echo "${TAG} Copying File: ${BASE_DIR}/Dockerfile"
+    cp Dockerfile.template ${BASE_DIR}/Dockerfile
+
+    echo "${TAG} Set Version: ${VERSION}"
+    sed -i '' -e "s/Z_VERSION=\"0.0.0\"/Z_VERSION=\"${VERSION}\"/g" ${BASE_DIR}/Dockerfile
+    sed -i '' -e "s/docker-zeppelin:tag/docker-zeppelin:${DOCKER_TAG}/g" ${BASE_DIR}/Dockerfile
 else
-    echo "${tag} Directory already exists: ${dirName}"
+    echo "${TAG} Directory already exists: ${BASE_DIR}"
 fi
 
 
