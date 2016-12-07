341d9e36cf98759e30cba1d0e35c996d86716302
1ambda <1amb4a@gmail.com>
2016-12-07 16:31:37 +0900

    fix: Directory structure

--- create-dockerfile.sh
+++ create-dockerfile.sh
@@ -17,21 +17,28 @@ if [[ ! "x${PLATFORM}" = "x" ]]; then
    POSTFIX="_${PLATFORM}"
 fi
 
-BASE_DIR="${LINUX}-${VERSION}${POSTFIX}"
-DOCKER_TAG="${LINUX}-${VERSION}${POSTFIX}"
+BASE_DIR="${LINUX}/base${POSTFIX}"
 
 if [ ! -d "$BASE_DIR" ]; then
-    echo "${TAG} Creating Directory: ${BASE_DIR}"
-    mkdir -p ${BASE_DIR}
+    echo "${TAG} Base Directory doesn't exist: ${BASE_DIR}"
+    exit 1
+fi
 
-    echo "${TAG} Copying File: ${BASE_DIR}/Dockerfile"
-    cp Dockerfile.template ${BASE_DIR}/Dockerfile
+DOCKER_DIR="${LINUX}/${VERSION}${POSTFIX}"
+DOCKER_TAG="${LINUX}-${VERSION}${POSTFIX}"
+
+if [ ! -d "$DOCKER_DIR" ]; then
+    echo "${TAG} Creating Directory: ${DOCKER_DIR}"
+    mkdir -p ${DOCKER_DIR}
+
+    echo "${TAG} Copying File: ${DOCKER_DIR}/Dockerfile"
+    cp Dockerfile.template ${DOCKER_DIR}/Dockerfile
 
     echo "${TAG} Set Version: ${VERSION}"
-    sed -i '' -e "s/Z_VERSION=\"0.0.0\"/Z_VERSION=\"${VERSION}\"/g" ${BASE_DIR}/Dockerfile
-    sed -i '' -e "s/docker-zeppelin:tag/docker-zeppelin:${DOCKER_TAG}/g" ${BASE_DIR}/Dockerfile
+    sed -i '' -e "s/Z_VERSION=\"0.0.0\"/Z_VERSION=\"${VERSION}\"/g" ${DOCKER_DIR}/Dockerfile
+    sed -i '' -e "s/docker-zeppelin:tag/docker-zeppelin:${DOCKER_TAG}/g" ${DOCKER_DIR}/Dockerfile
 else
-    echo "${TAG} Directory already exists: ${BASE_DIR}"
+    echo "${TAG} Directory already exists: ${DOCKER_DIR}"
 fi
 
 
